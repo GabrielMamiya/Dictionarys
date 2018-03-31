81ad361319faedb5381d546662053102c137b2b6
Gabriel Tamura <gabriel-tamura@hotmail.com>
2018-03-30 22:15:00 -0300

    Initial check-in of module Dictionarys

---
+++ Dictionarys/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionarys
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Dictionary<string, string> dicionario = new Dictionary<string, string>();
+
+            dicionario.Add("Mylena", "Gabriel");
+
+            foreach(var item in dicionario){
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
